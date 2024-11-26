4b94134ccfd4c3f0c97ad73e2fd1603e5a8fcb95
StrekalovRA <romikpistunov@gmail.com>
2024-11-26 22:31:39 +0500

    task 0, lib: 8 -> 9 option bruuuh

--- Tyuiu.StrekalovRA.Sprint5.Task0.V9.Lib/DataService.cs
+++ Tyuiu.StrekalovRA.Sprint5.Task0.V9.Lib/DataService.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 namespace Tyuiu.StrekalovRA.Sprint5.Task0.V9.Lib
 {
-    public class DataService : ISprint5Task0V8
+    public class DataService : ISprint5Task0V9
     {
         public string SaveToFileTextData(int x)
         {
