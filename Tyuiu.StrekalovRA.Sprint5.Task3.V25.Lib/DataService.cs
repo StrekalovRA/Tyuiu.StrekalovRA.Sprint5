96f0fa7a5d4c1b16d5423f07a4fb63305e60b304
StrekalovRA <romikpistunov@gmail.com>
2024-11-26 23:41:12 +0500

    task 3

--- Tyuiu.StrekalovRA.Sprint5.Task3.V25.Lib/DataService.cs
+++ Tyuiu.StrekalovRA.Sprint5.Task3.V25.Lib/DataService.cs
@@ -8,18 +8,18 @@ namespace Tyuiu.StrekalovRA.Sprint5.Task3.V25.Lib
     {
         public string SaveToFileTextData(int X)
         {
-            string Path = @$"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            string R_Path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
             double Y = (3 * Math.Pow(X, 4) + 1) / Math.Pow(X, 3);
             Y = Math.Round(Y, 3);
             string Y_ = Convert.ToString(Y);
 
-            using (BinaryWriter A = new BinaryWriter(File.Open(Path, FileMode.OpenOrCreate), Encoding.Unicode))
+            using (BinaryWriter A = new BinaryWriter(File.Open(R_Path, FileMode.OpenOrCreate), Encoding.Unicode))
             {
                 A.Write(BitConverter.GetBytes(Y));
             } // Что это такое... ☠☠☠
 
             //File.WriteAllText(Path, Y_);
-            return Path;
+            return R_Path;
         }
     }
 }
