fe2a4d6bc795ef8b8d97982ce9a3dba84b24a097
StrekalovRA <romikpistunov@gmail.com>
2024-11-27 10:24:40 +0500

    task 5 try 1

--- Tyuiu.StrekalovRA.Sprint5.Task5.V11.Lib/DataService.cs
+++ Tyuiu.StrekalovRA.Sprint5.Task5.V11.Lib/DataService.cs
@@ -1,7 +1,29 @@
-﻿namespace Tyuiu.StrekalovRA.Sprint5.Task5.V11.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text;
 
+namespace Tyuiu.StrekalovRA.Sprint5.Task5.V11.Lib
+{
+    public class DataService : ISprint5Task5V11
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string Nums = File.ReadAllText(path);
+            double Res = 1;
+
+            string[] New_Nums = Nums.Split("\n");
+            foreach (var I in New_Nums)
+            {
+                double Num_ = Convert.ToDouble(I);
+
+                if (Math.Abs(Num_) % 2 == 1)
+                {
+                    Res = Res * Num_;
+                    Console.WriteLine(I);
+                }
+            }
+            return Math.Round(Res, 3);
+        }
     }
 }
+
