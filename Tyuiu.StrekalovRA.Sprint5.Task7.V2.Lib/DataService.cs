e003809d2dae3c8361409b57d2af1affb829ff17
StrekalovRA <romikpistunov@gmail.com>
2024-11-27 10:58:47 +0500

    t7 t1

---
+++ Tyuiu.StrekalovRA.Sprint5.Task7.V2.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text;
+
+namespace Tyuiu.StrekalovRA.Sprint5.Task7.V2.Lib
+{
+    public class DataService : ISprint5Task7V2
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string Text = File.ReadAllText(path);
+            string New_ = "";
+
+            foreach ( char I in Text)
+            {
+                if (char.IsDigit(I))
+                {
+                    New_ += "#";
+                }
+                else
+                {
+                    New_ += I;
+                }
+            }
+
+            string R_Path = Path.Combine(Path.GetTempPath(), New_);
+            File.WriteAllText(R_Path, New_);
+            return R_Path;
+        }
+    }
+}
+
---
+++ Tyuiu.StrekalovRA.Sprint5.Task7.V2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.StrekalovRA.Sprint5.Task7.V2.Lib;
+using System.IO;
+
+namespace Tyuiu.StrekalovRA.Sprint5.Task5.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Стрекалов Р. А. | Смартб-24-1";
+            /*
+                Спринт #5
+                Тема: Потоковый метод записи данных в бинарный файл
+                Задание #7
+                Вариант #11
+                Выполнил: Стрекалов Роман Алексеевич | Смартб-24-1
+
+                УСЛОВИЕ:
+                Произвдение всех нечет чисел
+             */
+
+            //string A_Path = $@"{Directory.GetCurrentDirectory()}\InPutFileTask4.txt";
+            //string A_Path = @"T:\Big ProgramZ\VS_ProjectZ\Sprint5\DataSprint5\InPutDataFileTask5V25.txt";
+            string A_Path = Console.ReadLine();
+            //string A_Path = "r";
+
+            DataService DS = new DataService();
+            var T = DS.LoadDataAndSave(A_Path);
+
+            Console.WriteLine("РЕЗУЛЬТАТ: " + T);
+            Console.ReadKey();
+        }
+    }
+}
