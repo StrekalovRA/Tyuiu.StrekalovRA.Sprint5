17ec8e1e64bc62058477a98b3fe11f3dea1e044d
StrekalovRA <romikpistunov@gmail.com>
2024-11-27 09:50:36 +0500

    task 4 try 1

--- Tyuiu.StrekalovRA.Sprint5.Task4.V25.Lib/DataService.cs
+++ Tyuiu.StrekalovRA.Sprint5.Task4.V25.Lib/DataService.cs
@@ -1,7 +1,20 @@
-﻿namespace Tyuiu.StrekalovRA.Sprint5.Task4.V25.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text;
 
+namespace Tyuiu.StrekalovRA.Sprint5.Task4.V25.Lib
+{
+    public class DataService : ISprint5Task4V25
+    {
+        public double LoadFromDataFile (string path)
+        {
+            string Num_File = File.ReadAllText(path);
+            double Y = 0;
+            var X = Convert.ToDouble(Num_File);
+
+            Y = (Math.Pow(X, 4) + Math.Cos(X)) * Math.Sin(X);
+            Y = Math.Round(Y, 3);
+            return Y;
+        }
     }
 }
--- Tyuiu.StrekalovRA.Sprint5.Task4.V25/Program.cs
+++ Tyuiu.StrekalovRA.Sprint5.Task4.V25/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.StrekalovRA.Sprint5.Task4.V25.Lib;
+using System.IO;
+
+namespace Tyuiu.StrekalovRA.Sprint5.Task4.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Стрекалов Р. А. | Смартб-24-1";
+            /*
+                Спринт #5
+                Тема: Потоковый метод записи данных в бинарный файл
+                Задание #4
+                Вариант #25
+                Выполнил: Стрекалов Роман Алексеевич | Смартб-24-1
+
+                УСЛОВИЕ:
+                Файл -> X -> рез. в консоль
+                F(x) = (x^4 + cos(x))sin(x)
+             */
+
+            //string A_Path = $@"{Directory.GetCurrentDirectory()}\InPutFileTask4.txt";
+            //A_Path = @"T:\Big ProgramZ\VS_ProjectZ\Sprint5\DataSprint5\InPutDataFileTask4V25.txt";
+            string A_Path = Console.ReadLine();
+
+            DataService DS = new DataService();
+            var T = DS.LoadFromDataFile(A_Path);
+
+            Console.WriteLine("РЕЗУЛЬТАТ: " + T);
+            Console.ReadKey();
+        }
+    }
+}
